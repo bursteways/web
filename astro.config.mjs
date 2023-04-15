6de559095bfa0c226c9acecafe168938ebf73e08
bursteways <sburke0708@gmail.com>
2023-04-14 22:03:52 -0400

    starting styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 
-import sanity from 'astro-sanity';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    sanity({
-      projectId: 'tiaoeeth',
-      dataset: 'production',
-      apiVersion: '2021-03-25',
-      useCdn: true,
     }),
   ],
 });
