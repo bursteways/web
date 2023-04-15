8d5661c2efd2cdeca31b39c2b6def1b5cac856dd
bursteways <sburke0708@gmail.com>
2023-04-14 21:32:19 -0400

    cleanup

--- src/lib/urlForImage.ts
+++
@@ -1,7 +0,0 @@
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
-import { useSanityClient, createImageBuilder } from 'astro-sanity';
-
-export const imageBuilder = createImageBuilder(useSanityClient());
-
-export const urlForImage = (source: SanityImageSource) =>
-  imageBuilder.image(source);
